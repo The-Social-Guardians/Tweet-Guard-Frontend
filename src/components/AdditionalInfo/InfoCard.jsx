305e4e20fc7180c37838cf96cda13873585da677
Dunsin <jesudunsinfaiye@gmail.com>
2022-08-20 16:56:06 +0100

    feat(infocard): change the infomation of how this works

--- src/components/AdditionalInfo/InfoCard.jsx
+++ src/components/AdditionalInfo/InfoCard.jsx
@@ -1,20 +1,11 @@
-function InfoCard({ title, listContent }) {
+function InfoCard({ content, number }) {
   return (
     <article className="my-2 bg-neutral-50/10 p-2 lg:my-0 lg:mx-2 lg:w-1/2">
-      <h4 className="py-5 text-center text-xl font-semibold lg:text-2xl">
-        {title}
-      </h4>
-      <ul className="pl-7">
-        {listContent &&
-          listContent.map((listItem, index) => (
-            <li
-              className="my-4 list-outside list-disc leading-5 lg:text-lg lg:leading-7"
-              key={index}
-            >
-              {listItem}
-            </li>
-          ))}
-      </ul>
+      <div className="p-7">
+        <h2 className="text-2xl font-medium">Step {number + 1}</h2>
+        <div className="bg-primary h-px my-1.5"></div>
+        <p className="text-xl">{content}</p>
+      </div>
     </article>
   );
 }
