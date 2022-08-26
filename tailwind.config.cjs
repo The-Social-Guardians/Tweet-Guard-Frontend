ccab106d47f44419349d5ab141cee068be724e3f
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-08-26 16:16:29 +0530

    feat: Added animation to the hero section.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,6 +22,30 @@ module.exports = {
         secondary: colors.slate[900],
         popping: colors.sky[400],
       },
+
+      animation: {
+        "pulse-custom": "pulse-custom 1s ease-in-out infinite",
+      },
+
+      keyframes: {
+        "pulse-custom": {
+          "0%": {
+            "box-shadow": "0 0 0.25rem 0.25rem rgba(56, 189, 248, 1)",
+            transform: "scale(1.03)",
+          },
+
+          "50%": {
+            "box-shadow": "0 0 4rem 2rem rgba(56, 189, 248, 0.1)",
+            transform: "scale(1)",
+            opacity: 1,
+          },
+
+          "100%": {
+            "box-shadow": "none",
+            transform: "scale(0.97)",
+          },
+        },
+      },
     },
   },
   plugins: [],
